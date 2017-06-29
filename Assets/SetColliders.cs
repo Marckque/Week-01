f57189e896278369cd75c9c2717759a5ffadd9d8
Marc <m.bonte@rubika-edu.com>
2017-06-29 18:18:20 +0200

    Pan collider improved

---
+++ Assets/SetColliders.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColliders : MonoBehaviour
+{
+    [Range(0f, .1f)]
+    public float amplitude = 1f;
+    [Range(0f, 1f)]
+    public float offset;
+    private Transform[] children;
+
+    protected void Start()
+    {
+        Initialise();
+        SetAngles();
+    }
+
+    protected void OnValidate()
+    {
+        Initialise();
+        SetAngles();
+    }
+
+    protected void Initialise()
+    {
+        children = new Transform[transform.childCount];
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i] = transform.GetChild(i);
+        }
+    }
+
+    protected void SetAngles()
+    {
+        float angle = 360f / children.Length;
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            float x = amplitude * Mathf.Cos(angle) * Mathf.Rad2Deg;
+            float z = amplitude * Mathf.Sin(angle) * Mathf.Rad2Deg;
+
+            children[i].transform.position = new Vector3(x, children[i].transform.position.y, z);
+
+            children[i].LookAt(transform);
+
+            angle += offset;
+        }
+    }
+}
