4d5a9649fa272be5021e5a1e63f49f35df879e8d
Marc <m.bonte@rubika-edu.com>
2017-06-29 15:35:30 +0200

    Added sound management
    Sound changes according to pan's heat

--- Assets/ParticleHeat.cs
+++ Assets/ParticleHeat.cs
@@ -6,26 +6,46 @@ public class ParticleHeat : MonoBehaviour
 {
     public PanController panController;
     public ParticleSystem[] particleSystems;
+    public AudioSource[] audioSources;
 
     protected void Update()
     {
 		if (panController.heatingPower <= 0f)
         {
+            // VFX
             for (int i = 0; i < particleSystems.Length; i++)
             {
                 particleSystems[i].Stop();
             }
+
+            // SFX
+            for (int i = 0; i < audioSources.Length; i++)
+            {
+                audioSources[i].Stop();
+            }
         }
         else
         {
+            // VFX
             for (int i = 0; i < particleSystems.Length; i++)
             {
                 var em = particleSystems[i].emission;
-                em.rateOverTime = ExtensionMethods.Remap(panController.heatingPower, 0f, 5f, 0f, 20f);
+                em.rateOverTime = ExtensionMethods.Remap(panController.heatingPower, 0f, 5f, 0f, 40f);
 
                 if (!particleSystems[i].isPlaying)
                 {
                     particleSystems[i].Play();
+                }
+            }
+
+            // SFX
+            for (int i = 0; i < audioSources.Length; i++)
+            {
+                audioSources[i].volume = ExtensionMethods.Remap(panController.heatingPower, 0f, 5f, 0f, 0.5f);
+
+                if (!audioSources[i].isPlaying)
+                {
+                    audioSources[i].Play();
                 }
             }
         }
